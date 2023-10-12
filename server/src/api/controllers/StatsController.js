9b42021ab8007e2cadff85cc96792dc24f182478
leminhduc6402@gmail.com <leminhduc6402@gmail.com>
2023-10-12 13:42:43 +0700

    stats user post and post byLandlord

--- server/src/api/controllers/StatsController.js
+++ server/src/api/controllers/StatsController.js
@@ -5,6 +5,198 @@ import { Types } from "mongoose";
 
 const StatsController = {
   //[GET] /api/landlords/:id/stats?year=&month=&quarter...  => params { minPrice, maxPrice, minAcreage, maxAcreage, year, month, quarter } truyền trên đường dẫn
+  getStatsOfPostByLandlord: async (req, res, next) => {
+    try {
+      const { year, month, quarter } = req.query;
+      const query = {};
+      if (year && !month && !quarter) {
+        query.createdAt = {
+          $gte: new Date(year, 0, 2),
+          $lte: new Date(year, 12, 1),
+        };
+      }
+      if (month) {
+        query.createdAt = {
+          $gte: new Date(year, month - 1, 2),
+          $lte: new Date(year, month, 1),
+        };
+      } else if (quarter) {
+        const quarterStartMonth = (parseInt(quarter) - 1) * 3 + 1;
+        const quarterEndMonth = quarterStartMonth + 2;
+
+        const quarterStartDate = new Date(year, quarterStartMonth - 1, 2);
+
+        const quarterEndDate = new Date(year, quarterEndMonth, 1);
+
+        query.createdAt = {
+          $gte: quarterStartDate,
+          $lte: quarterEndDate,
+        };
+      }
+      const posts = await PostModel.aggregate([
+        {
+          $match: {
+            $expr: {
+              $eq: ["$userId", { $toObjectId: req.params.id }],
+            },
+            createdAt: query.createdAt,
+          },
+        },
+        {
+          $group: {
+            _id: {
+              year: { $year: "$createdAt" },
+              month: { $month: "$createdAt" },
+              day: { $dayOfMonth: "$createdAt" },
+            },
+            quantity: { $sum: 1 },
+          },
+        },
+        {
+          $group: {
+            _id: {
+              year: "$_id.year",
+              month: "$_id.month",
+            },
+            days: {
+              $push: {
+                day: "$_id.day",
+                quantity: "$quantity",
+              },
+            },
+            total: { $sum: "$quantity" },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            year: "$_id.year",
+            month: "$_id.month",
+            days: 1,
+            total: 1,
+          },
+        },
+        {
+          $group: {
+            _id: "$year",
+            months: {
+              $push: {
+                month: "$month",
+                days: "$days",
+                total: "$total",
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            year: "$_id",
+            months: 1,
+          },
+        },
+      ]);
+      return res.status(200).json(posts);
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  //[GET] /api/users/stats/ => { year, month, quarter }
+  getStatsOfUser: async (req, res, next) => {
+    try {
+      const { year, month, quarter } = req.query;
+      const query = {};
+      if (year && !month && !quarter) {
+        query.createdAt = {
+          $gte: new Date(year, 0, 1),
+          $lte: new Date(year, 11, 0),
+        };
+      }
+      if (month) {
+        query.createdAt = {
+          $gte: new Date(year, month - 1, 0),
+          $lte: new Date(year, month, 1),
+        };
+      } else if (quarter) {
+        const quarterStartMonth = (parseInt(quarter) - 1) * 3 + 1;
+        const quarterEndMonth = quarterStartMonth + 2;
+
+        const quarterStartDate = new Date(year, quarterStartMonth - 1, 2);
+
+        const quarterEndDate = new Date(year, quarterEndMonth, 1);
+
+        query.createdAt = {
+          $gte: quarterStartDate,
+          $lte: quarterEndDate,
+        };
+      }
+      const user = await UserModel.aggregate([
+        {
+          $match: query,
+        },
+        {
+          $group: {
+            _id: {
+              year: { $year: "$createdAt" },
+              month: { $month: "$createdAt" },
+              day: { $dayOfMonth: "$createdAt" },
+            },
+            quantity: { $sum: 1 },
+          },
+        },
+        {
+          $group: {
+            _id: {
+              year: "$_id.year",
+              month: "$_id.month",
+            },
+            days: {
+              $push: {
+                day: "$_id.day",
+                quantity: "$quantity",
+              },
+            },
+            total: { $sum: "$quantity" },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            year: "$_id.year",
+            month: "$_id.month",
+            days: 1,
+            total: 1,
+          },
+        },
+        {
+          $group: {
+            _id: "$year",
+            months: {
+              $push: {
+                month: "$month",
+                days: "$days",
+                total: "$total",
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            year: "$_id",
+            months: 1,
+          },
+        },
+      ]);
+      // const user = await UserModel.find(query);
+
+      return res.status(200).json(user);
+    } catch (error) {
+      return next(error);
+    }
+  },
+
   getStatsOfPost: async (req, res, next) => {
     try {
       const { year, month, quarter } = req.query;
@@ -33,44 +225,16 @@ const StatsController = {
           $lte: quarterEndDate,
         };
       }
-      // const posts = await PostModel.aggregate([
-      //   {
-      //     $match: {
-      //       $expr: {
-      //         $eq: ["$userId", { $toObjectId: req.params.id }],
-      //       },
-      //       createdAt: query.createdAt
-      //     },
-      //   },
-      //   {
-      //     $group: {
-      //       _id: { $month: "$createdAt" },
-      //       quantity: { $sum: 1 },
-      //     },
-      //   },
-      //   {
-      //     $project: {
-      //       _id: 0,
-      //       month: "$_id",
-      //       quantity: 1,
-      //     },
-      //   },
-      // ]);
       const posts = await PostModel.aggregate([
         {
-          $match: {
-            $expr: {
-              $eq: ["$userId", { $toObjectId: req.params.id }],
-            },
-            createdAt: query.createdAt,
-          },
+          $match: query
         },
         {
           $group: {
             _id: {
               year: { $year: "$createdAt" },
               month: { $month: "$createdAt" },
-              day: { $dayOfMonth: "$createdAt" }
+              day: { $dayOfMonth: "$createdAt" },
             },
             quantity: { $sum: 1 },
           },
@@ -79,16 +243,16 @@ const StatsController = {
           $group: {
             _id: {
               year: "$_id.year",
-              month: "$_id.month"
+              month: "$_id.month",
             },
             days: {
               $push: {
                 day: "$_id.day",
-                quantity: "$quantity"
-              }
+                quantity: "$quantity",
+              },
             },
-            total: { $sum: "$quantity" }
-          }
+            total: { $sum: "$quantity" },
+          },
         },
         {
           $project: {
@@ -96,8 +260,8 @@ const StatsController = {
             year: "$_id.year",
             month: "$_id.month",
             days: 1,
-            total: 1
-          }
+            total: 1,
+          },
         },
         {
           $group: {
@@ -106,68 +270,24 @@ const StatsController = {
               $push: {
                 month: "$month",
                 days: "$days",
-                total: "$total"
-              }
-            }
-          }
+                total: "$total",
+              },
+            },
+          },
         },
         {
           $project: {
             _id: 0,
             year: "$_id",
-            months: 1
-          }
-        }
+            months: 1,
+          },
+        },
       ]);
       return res.status(200).json(posts);
     } catch (error) {
       return next(error);
     }
-  },
-
-  //[GET] /api/users/stats/ => { year, month, quarter }
-  getStatsUser: async (req, res, next) => {
-    try {
-      const { year, month, quarter } = req.query;
-      const query = {};
-      if (year && !month && !quarter) {
-        query.createdAt = {
-          $gte: new Date(year, 0, 1),
-          $lte: new Date(year, 11, 0),
-        };
-      }
-      if (month) {
-        query.createdAt = {
-          $gte: new Date(year, month - 1, 0),
-          $lte: new Date(year, month, 1),
-        };
-      } else if (quarter) {
-        // 1 2 3 4
-        const quarterStartMonth = (parseInt(quarter) - 1) * 3 + 1; // [1, 4, 7, 10]
-        const quarterEndMonth = quarterStartMonth + 2;
-
-        // Tính ngày bắt đầu của quý
-        // const quarterStartDate = new Date(year, quarterStartMonth - 1, 1);
-        const quarterStartDate = new Date(year, quarterStartMonth - 1, 2);
-
-        // Tính ngày kết thúc của quý
-        // const quarterEndDate = new Date(year, quarterEndMonth - 1, 1);
-        const quarterEndDate = new Date(year, quarterEndMonth, 1);
-
-        // Sử dụng quarterStartDate và quarterEndDate trong truy vấn của bạn
-        query.createdAt = {
-          $gte: quarterStartDate,
-          $lte: quarterEndDate,
-        };
-      }
-
-      const user = await UserModel.find(query);
-
-      return res.status(200).json(user);
-    } catch (error) {
-      return next(error);
-    }
-  },
+  }
 };
 export default StatsController;
 
--- server/src/api/routes/StatsRoute.js
+++ server/src/api/routes/StatsRoute.js
@@ -5,7 +5,16 @@ import { verifyAdmin, verifyLandlord } from "../middlewares/verifyToken.js";
 const router = Router();
 
 // thống kê bài viết của chủ trọ. id truyển trên đường dẫn là id của người dùng có vai trò landlord
-router.get("/landlords/:id/stats",verifyLandlord, StatsController.getStatsOfPost); 
-router.get("/users/admin/stats", verifyAdmin, StatsController.getStatsUser)
+router.get(
+  "/landlords/:id/stats",
+  verifyLandlord,
+  StatsController.getStatsOfPostByLandlord
+);
+
+// Thống kê số lượng người dùng đã đăng ký theo năm, quý, tháng
+router.get("/admin/users/stats", verifyAdmin, StatsController.getStatsOfUser);
+
+// Thống kê số lượng bài viết theo năm, quý, tháng
+router.get("/admin/posts/stats", verifyAdmin, StatsController.getStatsOfPost);
 
 export default router;
